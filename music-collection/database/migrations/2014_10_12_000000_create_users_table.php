b9645ba2dbd980a51f58956c5a2aa48b8b541962
rnodev <50058441+rnodev@users.noreply.github.com>
2021-05-14 16:42:10 -0300

    Add column for control the role of each user

--- music-collection/database/migrations/2014_10_12_000000_create_users_table.php
+++ music-collection/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->unique();//username
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->enum('role',['user', 'admin']);
         });
     }
 
