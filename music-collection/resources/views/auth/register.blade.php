f4af6d4109ec0ba0d6184efbfb22ca6011db427b
rnodev <50058441+rnodev@users.noreply.github.com>
2021-05-14 16:49:59 -0300

    add fiel for role of the user

--- music-collection/resources/views/auth/register.blade.php
+++ music-collection/resources/views/auth/register.blade.php
@@ -40,6 +40,24 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="role" class="col-md-4 col-form-label text-md-right">{{ __('Role') }}</label>
+                            <div class="col-md-6">
+                                <select class="form-control @error('role') is-invalid @enderror" id="role" name="role" required data-live-search="true" >
+                                    <option value=""> --Select Role-- </option>
+                                    <option value="user" {{(old('role')=='user')?'selected':''}}>User</option>
+                                    <option value="admin" {{(old('role')=='admin')?'selected':''}}>Admin</option>
+                                </select>
+
+                                @error('role')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+
+                        <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
