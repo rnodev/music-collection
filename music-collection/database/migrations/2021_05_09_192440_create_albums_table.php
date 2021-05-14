5b5435c451e3c88bd19b60ddd5ef183a78397554
rnodev <50058441+rnodev@users.noreply.github.com>
2021-05-14 16:45:33 -0300

    Album migration

---
+++ music-collection/database/migrations/2021_05_09_192440_create_albums_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->id();
+            $table->string('artist');
+            $table->string('artist_id');
+            $table->string('album_name');
+            $table->integer('year');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
